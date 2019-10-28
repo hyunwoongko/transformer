3450b67645761e359d9676cc9641046ccbf72a0e
gusdnd852 <gusdnd852@naver.com>
2019-10-29 02:12:24 +0900

    change some code

--- models/embedding/positional_encoding.py
+++ models/embedding/positional_encoding.py
@@ -34,8 +34,8 @@ class PostionalEncoding(nn.Module):
         # 'i' means index of d_model (e.g. embedding size = 50, 'i' = [0,50])
         # "step=2" means 'i' multiplied with two (same with 2 * i)
 
-        self.encoding[:, 0::2] = torch.sin(pos / 10000 ** (_2i / d_model))
-        self.encoding[:, 1::2] = torch.cos(pos / 10000 ** (_2i / d_model))
+        self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
+        self.encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
         # compute positional encoding to consider positional information of words
 
     def forward(self, x):
