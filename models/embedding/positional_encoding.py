c2592c96bf0b673c93693e01e571ccffc43d347c
gusdnd852 <gusdnd852@naver.com>
2019-10-29 02:10:12 +0900

    change code more readable

--- models/embedding/positional_encoding.py
+++ models/embedding/positional_encoding.py
@@ -33,19 +33,17 @@ class PostionalEncoding(nn.Module):
         # 'i' means index of d_model (e.g. embedding size = 50, 'i' = [0,50])
         # "step=2" means 'i' multiplied with two (same with 2 * i)
 
-        pos = pos / 10000 ** (_2i / d_model)
-        # compute position information (same with original paper)
-
-        self.encoding[:, 0::2] = torch.sin(pos)  # if 'i' is even [0, 2, 4, ... ] => sin
-        self.encoding[:, 1::2] = torch.cos(pos)  # if 'i' is odd [1, 3, 5, ... ] => cos
+        self.encoding[:, 0::2] = torch.sin(pos / 10000 ** (_2i / d_model))
+        self.encoding[:, 1::2] = torch.cos(pos / 10000 ** (_2i / d_model))
+        # compute positional encoding to consider positional information of words
 
     def forward(self, x):
         # self.encoding
         # [max_len = 512, d_model = 512]
 
         batch_size, seq_len = x.size()
-        # [batch_size = 128, seq_len = 31]
+        # [batch_size = 128, seq_len = 30]
 
         return self.encoding[:seq_len, :]
-        # [seq_len = 31, d_model = 512]
-        # it will add tok_emb : [128, 31, 512]
+        # [seq_len = 30, d_model = 512]
+        # it will add tok_emb : [128, 30, 512]
