eda46149f57b48029ff1516b0e9d5aaf4865263c
gusdnd852 <gusdnd852@naver.com>
2019-10-29 02:18:03 +0900

    change some code

--- models/embedding/positional_encoding.py
+++ models/embedding/positional_encoding.py
@@ -23,8 +23,8 @@ class PostionalEncoding(nn.Module):
         super(PostionalEncoding, self).__init__()
 
         # same size with input matrix (for adding with input matrix)
-        self.encoding = torch.zeros(max_len, d_model, device=device)
-        self.encoding.requires_grad = False  # we don't need to grad
+        self.PE = torch.zeros(max_len, d_model, device=device)
+        self.PE.requires_grad = False  # we don't need to grad
 
         pos = torch.arange(0, max_len, device=device)
         pos = pos.float().unsqueeze(dim=1)
@@ -34,17 +34,17 @@ class PostionalEncoding(nn.Module):
         # 'i' means index of d_model (e.g. embedding size = 50, 'i' = [0,50])
         # "step=2" means 'i' multiplied with two (same with 2 * i)
 
-        self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
-        self.encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
+        self.PE[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
+        self.PE[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
         # compute positional encoding to consider positional information of words
 
     def forward(self, x):
-        # self.encoding
+        # self.PE : Positional Encoding
         # [max_len = 512, d_model = 512]
 
         batch_size, seq_len = x.size()
         # [batch_size = 128, seq_len = 30]
 
-        return self.encoding[:seq_len, :]
+        return self.PE[:seq_len, :]
         # [seq_len = 30, d_model = 512]
         # it will add with tok_emb : [128, 30, 512]
