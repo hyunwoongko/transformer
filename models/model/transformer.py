c74a837778a22346fcbf7734046d11539a401c07
gusdnd852 <gusdnd852@naver.com>
2019-12-18 12:41:25 +0900

    change mask codes

--- models/model/transformer.py
+++ models/model/transformer.py
@@ -51,7 +51,7 @@ class Transformer(nn.Module):
     def make_trg_mask(self, trg):
         trg_pad_mask = (trg != self.trg_pad_idx).unsqueeze(1).unsqueeze(3)
         trg_len = trg.shape[1]
-        trg_sub_mask = torch.tril(torch.ones((trg_len, trg_len), device=self.device)).type(torch.ByteTensor).to(self.device)
+        trg_sub_mask = torch.tril(torch.ones(trg_len, trg_len)).type(torch.ByteTensor).to(self.device)
         trg_mask = trg_pad_mask & trg_sub_mask
 
         return trg_mask
